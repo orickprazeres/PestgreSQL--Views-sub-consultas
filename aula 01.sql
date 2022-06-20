11df4636d3bd7f0ad0ddc50444377a1e083f49ae
Ricardo Prazeres <ricardoprazeres@hotmail.com>
2022-06-20 19:25:02 -0300

    Update aula 01.sql

--- aula 01.sql
+++ aula 01.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE alura;
 
+-- criando tabela Aluno
 CREATE TABLE aluno (
     id SERIAL PRIMARY KEY,
 	primeiro_nome VARCHAR(255) NOT NULL,
@@ -7,17 +8,20 @@ CREATE TABLE aluno (
 	data_nascimento DATE NOT NULL
 );
 
+-- criando tabela Categoria dos cursos
 CREATE TABLE categoria (
     id SERIAL PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL UNIQUE
 );
 
+-- criando tabela de Cursos
 CREATE TABLE curso (
     id SERIAL PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL,
 	categoria_id INTEGER NOT NULL REFERENCES categoria(id)
 );
 
+-- criando tabela estrangeira de ALUNO/CURSO
 CREATE TABLE aluno_curso (
 	aluno_id INTEGER NOT NULL REFERENCES aluno(id),
 	curso_id INTEGER NOT NULL REFERENCES curso(id),
